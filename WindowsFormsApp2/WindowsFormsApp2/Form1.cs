8a98c77cef076c53a1acc78e3465c8e816a13b78
Kamila1801 <81405496+Kamila1801@users.noreply.github.com>
2021-03-26 09:42:23 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    
+    public partial class Form1 : Form
+    {
+        // открытое статическое свойство для хранения объект Form1
+        // используется для возврата к этой форме из других форм
+        public static Form1 FORMA
+        {
+            get; set;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // метод показа формы Регистрации
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // закрываем первую форму и заканчиваем работу программы
+            this.Close();
+            // или можно по другому
+            // Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // создаем форму регистрации Form2 
+            Form2 f = new Form2();
+            // сохраняем ссылку на текущую форму – Form1
+            FORMA = this;
+            // текущую форму прячем 
+            this.Hide();
+            f.Show();
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        // закрытое поле, которое хранит объект модели 
+        // для использования данных о пользователях в методах класса
+        Model1 db = new Model1();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // форму регистрации закрываем
+            this.Close();
+            // показываем скрытую форму Form1
+            Form1.FORMA.Show();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // проверяем, что во все текстовые поля введены данные
+            if (textBox1.Text == "" || textBox2.Text == "" ||
+              textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "")
+            {
+                MessageBox.Show("Нужно задать все данные!");
+                return;
+            }
+            if (textBox2.Text != textBox3.Text)
+            {
+                MessageBox.Show("Значения паролей не совпадают!");
+                return;
+            }
+            if ((textBox4.Text != "Директор") && (textBox4.Text != "Бухгалтер") 
+                                        && (textBox4.Text != "Заказчик"))
+  {
+                MessageBox.Show("Задана неверная роль!");
+                return;
+            }
+
+            // ищем запись пользователя с введенным логином
+            users usr = db.users.Find(textBox1.Text);
+            // если такой пользователь есть и его пароль правильный
+            if (usr != null)
+            {
+                MessageBox.Show("Пользователь с таким логином уже есть!");
+                return;
+            }
+            // создаем объект Users для нового пользователя
+            usr = new users();
+            usr.login = textBox1.Text;
+            usr.psw = textBox2.Text;
+            usr.role = textBox3.Text;
+            usr.name = textBox5.Text;
+            // добавляем новый объект в коллекцию объектов Users
+            db.users.Add(usr);
+            try
+            {
+                // сохраняем нового пользователя в базе данных
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            MessageBox.Show("Пользователь " + usr.login + "зарегистрирован!");
+            Form1.FORMA.Show();
+            this.Close();
+            return;
+
+        }
+    }
+}
